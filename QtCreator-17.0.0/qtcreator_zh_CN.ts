0ac126c681e4603f089db5096be64d2ffb8f1270
ZYZ <497074117@qq.com>
2025-07-28 09:55:05 +0800

    增加翻译 1.增加Minimap插件的翻译

--- QtCreator-17.0.0/qtcreator_zh_CN.ts
+++ QtCreator-17.0.0/qtcreator_zh_CN.ts
@@ -64566,7 +64566,7 @@ Will not be applied to whitespace in comments and strings.</source>
     </message>
     <message>
         <source>Line: %1, Col: %2</source>
-        <translation>行号: %1, 列号: %2</translation>
+        <translation>行: %1, 列: %2</translation>
     </message>
     <message>
         <source>Cursor position: %1</source>
@@ -76000,6 +76000,81 @@ Please ensure the selected URL is accessible and the service is running.</source
         <location filename="../../doxygensettings.cpp" line="46"/>
         <source>Doxygen</source>
         <translation>Doxygen</translation>
+    </message>
+</context>
+<context>
+    <name>QtC::Minimap</name>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="70"/>
+        <location filename="../../minimapsettings.cpp" line="171"/>
+        <source>Minimap</source>
+        <translation>Minimap</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="74"/>
+        <source>Check to enable Minimap scrollbar</source>
+        <translation>选中以启用Minimap滚动条</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="76"/>
+        <source>Enabled:</source>
+        <translation>启用：</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="80"/>
+        <source>The width of the Minimap</source>
+        <translation>Minimap宽度</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="82"/>
+        <source>Width:</source>
+        <translation>宽度：</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="87"/>
+        <source>Line count threshold where no Minimap scrollbar is to be used</source>
+        <translation>不使用Minimap滚动条的行数阈值</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="89"/>
+        <source>Line Count Threshold:</source>
+        <translation>行数阈值：</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="93"/>
+        <source>The alpha value of the scrollbar slider</source>
+        <translation>滚动条滑块的alpha值</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="95"/>
+        <source>Scrollbar slider alpha value:</source>
+        <translation>滚动条滑块alpha值：</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="98"/>
+        <source>Center viewport on mouse position when clicking and dragging</source>
+        <translation>单击和拖动时鼠标位置的中心视窗</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="100"/>
+        <source>Center on click:</source>
+        <translation>点击居中：</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="103"/>
+        <source>Show line range tooltip when interacting with minimap</source>
+        <translation>当与minimap交互时显示行范围提示信息</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="105"/>
+        <source>Show line tooltip:</source>
+        <translation>显示行提示信息：</translation>
+    </message>
+    <message>
+        <location filename="../../minimapsettings.cpp" line="109"/>
+        <location filename="../../minimapsettings.cpp" line="152"/>
+        <source>Disable text wrapping to enable Minimap scrollbar</source>
+        <translation>禁用文本换行以启用Minimap滚动条</translation>
     </message>
 </context>
 </TS>
