30ada2c0fbb63d1f61fdc8901e86981cb2a39e45
ZYZ <497074117@qq.com>
2025-07-28 08:55:36 +0800

    增加翻译 1.修改几处翻译 2.增加DoxygenPlugin插件的翻译

---
+++ DoxygenPlugin-17.0.0/DoxygenPlugin_zh_CN.ts
@@ -0,0 +1,316 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_US">
+<context>
+    <name>DoxyPlugin::Internal::Doxygen</name>
+    <message>
+        <location filename="../../doxygen.cpp" line="534"/>
+        <source>Doxygen</source>
+        <translation>Doxygen</translation>
+    </message>
+    <message>
+        <location filename="../../doxygen.cpp" line="535"/>
+        <source>You don&apos;t have any current project.</source>
+        <translation>你目前没有任何项目。</translation>
+    </message>
+</context>
+<context>
+    <name>DoxyPlugin::Internal::DoxygenPlugin</name>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="109"/>
+        <source>&amp;Doxygen</source>
+        <translation>&amp;Doxygen</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="115"/>
+        <source>Document current entity</source>
+        <translation>当前实体生成文档</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="118"/>
+        <source>Ctrl+Shift+F9</source>
+        <translation>Ctrl+Shift+F9</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="126"/>
+        <source>Document current file</source>
+        <translation>当前文件生成文档</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="129"/>
+        <source>Ctrl+Shift+F5</source>
+        <translation>Ctrl+Shift+F5</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="144"/>
+        <source>Document current project</source>
+        <translation>当前项目生成文档</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="148"/>
+        <source>Ctrl+Shift+F8</source>
+        <translation>Ctrl+Shift+F8</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="154"/>
+        <source>Build Doxygen Documentation</source>
+        <translation>构建Doxygen文档</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="157"/>
+        <source>Ctrl+Shift+F4</source>
+        <translation>Ctrl+Shift+F4</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="162"/>
+        <source>Edit Doxyfile</source>
+        <translation>编辑Doxyfile</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="165"/>
+        <source>Ctrl+Shift+F6</source>
+        <translation>Ctrl+Shift+F6</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="239"/>
+        <location filename="../../doxygenplugin.cpp" line="273"/>
+        <source>Doxygen</source>
+        <translation>Doxygen</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="240"/>
+        <location filename="../../doxygenplugin.cpp" line="274"/>
+        <source>You don&apos;t have any current project.</source>
+        <translation>你目前没有任何项目。</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="286"/>
+        <source>Failed to launch %1
+</source>
+        <translation>启动%1失败
+</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="295"/>
+        <source>No doxygen executable specified</source>
+        <translation>没有指定doxygen可执行文件</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="299"/>
+        <source>Executing: %1 %2
+</source>
+        <translation>正在执行: %1 %2
+</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="330"/>
+        <source>The process terminated with exit code %1.</source>
+        <translation>进程以退出码%1终止。</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenplugin.cpp" line="336"/>
+        <source>Doxygen ran successfully</source>
+        <translation>Doxygen运行成功</translation>
+    </message>
+</context>
+<context>
+    <name>DoxygenFilesDialog</name>
+    <message>
+        <location filename="../../doxygenfilesdialog.ui" line="17"/>
+        <source>Select files to document</source>
+        <translation>选择文件到文档</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenfilesdialog.ui" line="53"/>
+        <source>Select All</source>
+        <translation>选择全部</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenfilesdialog.ui" line="60"/>
+        <source>Select None</source>
+        <translation>取消选择</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenfilesdialog.ui" line="80"/>
+        <source>Ok</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <location filename="../../doxygenfilesdialog.ui" line="87"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+</context>
+<context>
+    <name>DoxygenSettingsWidget</name>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="32"/>
+        <source>Doxygen Settings</source>
+        <translation>Doxygen设置</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="47"/>
+        <source>Doxygen Configuration</source>
+        <translation>Doxygen配置</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="59"/>
+        <source>Doxyfile filename (should be at project root):</source>
+        <translation>Doxyfile文件名（应该在项目根目录下）：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="76"/>
+        <source>Doxygen command:</source>
+        <translation>Doxygen命令：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="96"/>
+        <source>DoxyWizard comand:</source>
+        <translation>DoxyWizard命令：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="122"/>
+        <source>Comments Settings</source>
+        <translation>注释设置</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="133"/>
+        <source>Comments style:</source>
+        <translation>注释样式：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="144"/>
+        <source>JavaDoc</source>
+        <translation>JavaDoc</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="149"/>
+        <source>Qt</source>
+        <translation>Qt</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="154"/>
+        <source>Custom</source>
+        <translation>自定义</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="162"/>
+        <source>Files to comment:</source>
+        <translation>要注释的文件：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="170"/>
+        <source>Headers</source>
+        <translation>Headers</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="175"/>
+        <source>Implementations</source>
+        <translation>实现</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="180"/>
+        <source>All</source>
+        <translation>全部</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="188"/>
+        <source>Print brief tag</source>
+        <translation>打印简要标签</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="202"/>
+        <source>Short Variable documentation: </source>
+        <translation>短变量文档： </translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="216"/>
+        <source>Verbose (fn, var, class file...): </source>
+        <translation>详细（fn, var，类文件…）： </translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="230"/>
+        <source>Comment files</source>
+        <translation>注释文件</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="244"/>
+        <source>Comment headers</source>
+        <translation>注释headers</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="258"/>
+        <source>Comment implementation</source>
+        <translation>注释实现</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="272"/>
+        <source>Automatically add return type: </source>
+        <translation>自动添加返回类型： </translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="292"/>
+        <source>File comments Settings</source>
+        <translation>文件注释设置</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="302"/>
+        <source>Cutom comments Settings</source>
+        <translation>自定义注释设置</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="311"/>
+        <source>Begin tag:</source>
+        <translation>开始标签：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="328"/>
+        <source>Brief tag:</source>
+        <translation>简要标签：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="345"/>
+        <source>Empty line tag:</source>
+        <translation>空行标签：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="362"/>
+        <source>New line tag:</source>
+        <translation>新行标签：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="379"/>
+        <source>End tag:</source>
+        <translation>结束标签：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="396"/>
+        <source>Short doc tag:</source>
+        <translation>短文档标签：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.ui" line="413"/>
+        <source>Short doc end:</source>
+        <translation>短文档结束：</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.cpp" line="31"/>
+        <source>Doxygen Command</source>
+        <translation>Doxygen命令</translation>
+    </message>
+    <message>
+        <location filename="../../doxygensettingswidget.cpp" line="33"/>
+        <source>DoxyWizard Command</source>
+        <translation>DoxyWizard命令</translation>
+    </message>
+</context>
+<context>
+    <name>QtC::DoxygenPlugin</name>
+    <message>
+        <location filename="../../doxygensettings.cpp" line="40"/>
+        <location filename="../../doxygensettings.cpp" line="46"/>
+        <source>Doxygen</source>
+        <translation>Doxygen</translation>
+    </message>
+</context>
+</TS>
