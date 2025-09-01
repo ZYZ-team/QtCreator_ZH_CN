d1eae56be9e89d739cf702bbb8ceb26398320def
ZYZ <497074117@qq.com>
2025-09-01 09:59:07 +0800

    QtCreator版本升级到17.0.1
    Signed-off-by: ZYZ <497074117@qq.com>

---
+++ QtCreator-17.0.1/DoxygenPlugin_zh_CN-17.0.0.ts
@@ -0,0 +1,255 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_US">
+<context>
+    <name>DoxyPlugin::Internal::Doxygen</name>
+    <message>
+        <source>Doxygen</source>
+        <translation>Doxygen</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have any current project.</source>
+        <translation>你目前没有任何项目。</translation>
+    </message>
+</context>
+<context>
+    <name>DoxyPlugin::Internal::DoxygenPlugin</name>
+    <message>
+        <source>&amp;Doxygen</source>
+        <translation>&amp;Doxygen</translation>
+    </message>
+    <message>
+        <source>Document current entity</source>
+        <translation>当前实体生成文档</translation>
+    </message>
+    <message>
+        <source>Ctrl+Shift+F9</source>
+        <translation>Ctrl+Shift+F9</translation>
+    </message>
+    <message>
+        <source>Document current file</source>
+        <translation>当前文件生成文档</translation>
+    </message>
+    <message>
+        <source>Ctrl+Shift+F5</source>
+        <translation>Ctrl+Shift+F5</translation>
+    </message>
+    <message>
+        <source>Document current project</source>
+        <translation>当前项目生成文档</translation>
+    </message>
+    <message>
+        <source>Ctrl+Shift+F8</source>
+        <translation>Ctrl+Shift+F8</translation>
+    </message>
+    <message>
+        <source>Build Doxygen Documentation</source>
+        <translation>构建Doxygen文档</translation>
+    </message>
+    <message>
+        <source>Ctrl+Shift+F4</source>
+        <translation>Ctrl+Shift+F4</translation>
+    </message>
+    <message>
+        <source>Edit Doxyfile</source>
+        <translation>编辑Doxyfile</translation>
+    </message>
+    <message>
+        <source>Ctrl+Shift+F6</source>
+        <translation>Ctrl+Shift+F6</translation>
+    </message>
+    <message>
+        <source>Doxygen</source>
+        <translation>Doxygen</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have any current project.</source>
+        <translation>你目前没有任何项目。</translation>
+    </message>
+    <message>
+        <source>Failed to launch %1
+</source>
+        <translation>启动%1失败
+</translation>
+    </message>
+    <message>
+        <source>No doxygen executable specified</source>
+        <translation>没有指定doxygen可执行文件</translation>
+    </message>
+    <message>
+        <source>Executing: %1 %2
+</source>
+        <translation>正在执行: %1 %2
+</translation>
+    </message>
+    <message>
+        <source>The process terminated with exit code %1.</source>
+        <translation>进程以退出码%1终止。</translation>
+    </message>
+    <message>
+        <source>Doxygen ran successfully</source>
+        <translation>Doxygen运行成功</translation>
+    </message>
+</context>
+<context>
+    <name>DoxygenFilesDialog</name>
+    <message>
+        <source>Select files to document</source>
+        <translation>选择文件到文档</translation>
+    </message>
+    <message>
+        <source>Select All</source>
+        <translation>选择全部</translation>
+    </message>
+    <message>
+        <source>Select None</source>
+        <translation>取消选择</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+</context>
+<context>
+    <name>DoxygenSettingsWidget</name>
+    <message>
+        <source>Doxygen Settings</source>
+        <translation>Doxygen设置</translation>
+    </message>
+    <message>
+        <source>Doxygen Configuration</source>
+        <translation>Doxygen配置</translation>
+    </message>
+    <message>
+        <source>Doxyfile filename (should be at project root):</source>
+        <translation>Doxyfile文件名（应该在项目根目录下）：</translation>
+    </message>
+    <message>
+        <source>Doxygen command:</source>
+        <translation>Doxygen命令：</translation>
+    </message>
+    <message>
+        <source>DoxyWizard comand:</source>
+        <translation>DoxyWizard命令：</translation>
+    </message>
+    <message>
+        <source>Comments Settings</source>
+        <translation>注释设置</translation>
+    </message>
+    <message>
+        <source>Comments style:</source>
+        <translation>注释样式：</translation>
+    </message>
+    <message>
+        <source>JavaDoc</source>
+        <translation>JavaDoc</translation>
+    </message>
+    <message>
+        <source>Qt</source>
+        <translation>Qt</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>自定义</translation>
+    </message>
+    <message>
+        <source>Files to comment:</source>
+        <translation>要注释的文件：</translation>
+    </message>
+    <message>
+        <source>Headers</source>
+        <translation>头文件</translation>
+    </message>
+    <message>
+        <source>Implementations</source>
+        <translation>实现</translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation>全部</translation>
+    </message>
+    <message>
+        <source>Print brief tag</source>
+        <translation>打印简要标签</translation>
+    </message>
+    <message>
+        <source>Short Variable documentation: </source>
+        <translation>短变量文档： </translation>
+    </message>
+    <message>
+        <source>Verbose (fn, var, class file...): </source>
+        <translation>详细（fn, var，类文件…）： </translation>
+    </message>
+    <message>
+        <source>Comment files</source>
+        <translation>注释文件</translation>
+    </message>
+    <message>
+        <source>Comment headers</source>
+        <translation>注释headers</translation>
+    </message>
+    <message>
+        <source>Comment implementation</source>
+        <translation>注释实现</translation>
+    </message>
+    <message>
+        <source>Automatically add return type: </source>
+        <translation>自动添加返回类型： </translation>
+    </message>
+    <message>
+        <source>File comments Settings</source>
+        <translation>文件注释设置</translation>
+    </message>
+    <message>
+        <source>Cutom comments Settings</source>
+        <translation>自定义注释设置</translation>
+    </message>
+    <message>
+        <source>Begin tag:</source>
+        <translation>开始标签：</translation>
+    </message>
+    <message>
+        <source>Brief tag:</source>
+        <translation>简要标签：</translation>
+    </message>
+    <message>
+        <source>Empty line tag:</source>
+        <translation>空行标签：</translation>
+    </message>
+    <message>
+        <source>New line tag:</source>
+        <translation>新行标签：</translation>
+    </message>
+    <message>
+        <source>End tag:</source>
+        <translation>结束标签：</translation>
+    </message>
+    <message>
+        <source>Short doc tag:</source>
+        <translation>短文档标签：</translation>
+    </message>
+    <message>
+        <source>Short doc end:</source>
+        <translation>短文档结束：</translation>
+    </message>
+    <message>
+        <source>Doxygen Command</source>
+        <translation>Doxygen命令</translation>
+    </message>
+    <message>
+        <source>DoxyWizard Command</source>
+        <translation>DoxyWizard命令</translation>
+    </message>
+</context>
+<context>
+    <name>QtC::DoxygenPlugin</name>
+    <message>
+        <source>Doxygen</source>
+        <translation>Doxygen</translation>
+    </message>
+</context>
+</TS>
---
+++ QtCreator-17.0.1/合并ts生成qm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/opt/Qt/6.9.1/gcc_64/bin/lconvert -i qtcreator_zh_CN-17.0.1.ts QodeAssist_zh_CN-0.6.1.ts MinimapPlugin_zh_CN-17.0.0.ts DoxygenPlugin_zh_CN-17.0.0.ts -o qtcreator_zh_CN.ts
+
+/opt/Qt/6.9.1/gcc_64/bin/lrelease qtcreator_zh_CN.ts -qm qtcreator_zh_CN.qm
+
+rm -rf qtcreator_zh_CN.ts
